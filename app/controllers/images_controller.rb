efbf0ac4ee91607f978aba0ac2bfb2fce87f0ad0
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-12 16:27:20 -0400

    Refactoring in the ImagesController + migration legend as text

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,49 +1,44 @@
 class ImagesController < ApplicationController
-#  before_action :image_params
-skip_before_action :authenticate_user!, only: :index
+  before_action :image_params, only: [:show, :update, :destroy, :edit]
+  skip_before_action :authenticate_user!, only: :index
 
   def index
-   @images = Image.all
+    @images = Image.all
+  end
+    
+  def show; end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    Image.create(image_params)
+    redirect_to images_path
+  end
+
+  def edit; end  
+
+  def update
+    @image.update(image_params)
+    redirect_to images_path 
+  end
+
+  def destroy
+    @image.destroy
+    redirect_to images_path
   end
   
-  def show
-  @image = Image.find(params[:id])
-end
-
-def new
-  @image = Image.new
-end
-
-def create
-  Image.create(image_params)
-  redirect_to images_path
-end
-
-def edit
-  @image = Image.find(params[:id])
-end  
-
-def update
-  @image = Image.find(params[:id])
-  @image.update(image_params)
-  redirect_to images_path
+  private
   
-end
-
-def destroy
-  @image = Image.find(params[:id])
-  @image.destroy
-  redirect_to images_path
+  def set_image
+    @image = Image.find(params[:id])
+  end
   
-end
-
-private
-
-# Strong parameters
-
-def image_params
-  params.require(:image).permit(:title, :rating, :legend, :path, :photo)
-end
+  # Strong parameters
+  def image_params
+    params.require(:image).permit(:title, :rating, :legend, :path, :photo)
+  end
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_231855) do
+ActiveRecord::Schema.define(version: 2021_05_12_201823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2021_05_04_231855) do
   create_table "images", force: :cascade do |t|
     t.string "title"
     t.string "path"
-    t.string "legend"
+    t.text "legend"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
