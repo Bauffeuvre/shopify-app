7f9feeeeceb817d493d575f528a7bfe4bd0576a9
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-15 08:50:29 -0400

    Added reviews method + form + display on the images/show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_123247) do
+ActiveRecord::Schema.define(version: 2021_05_15_124145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2021_05_15_123247) do
     t.bigint "image_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating"
+    t.text "content"
     t.index ["image_id"], name: "index_reviews_on_image_id"
   end
 
