7fbf3b3ea75a32c78ceb5f9665bc83dbf2b58731
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-16 17:53:20 -0400

    tests

--- test/system/images_test.rb
+++ test/system/images_test.rb
@@ -19,32 +19,32 @@ class ImagesTest < ApplicationSystemTestCase
     assert_selector "h5", text: "Test Title"
   end
   
-  # test "lets a signed in user create a new product with a link" do
-  #   login_as users(:baptiste)
-  #   visit "/images/new"
-  #   debugger
-  #   # save_and_open_screenshot
-  #   find_field("Title of the image").set("Image Title Test")
-  #   fill_in "which url ?", with: "https://www.mairie-francheville69.fr/wp-content/uploads/2017/11/image-test-1.jpeg"
-  #   fill_in "What legend ?", with: "Image Legend Test"
-  #   fill_in "How many stars?", with: "5"
-  #   click_on 'Send the image'
-  #   # Should be redirected to Home with new Image
-  #   assert_equal root_path, page.current_path
-  #   assert_selector "Image Title Test"
-  # end
+  test "lets a signed in user create a new product with a link" do
+    login_as users(:baptiste)
+    visit "/images/new"
+    debugger
+    # save_and_open_screenshot
+    find_field("Title of the image").set("Image Title Test")
+    fill_in "which url ?", with: "https://www.mairie-francheville69.fr/wp-content/uploads/2017/11/image-test-1.jpeg"
+    fill_in "What legend ?", with: "Image Legend Test"
+    fill_in "How many stars?", with: "5"
+    click_on 'Send the image'
+    # Should be redirected to Home with new Image
+    assert_equal root_path, page.current_path
+    assert_selector "Image Title Test"
+  end
 
-  # test "lets a signed in user create a new product with a file" do
-  #   login_as users(:baptiste)
-  #   visit "/images/new"
-  #   fill_in "title", with: "Image Title Test"
-  #   fill_in "path", with: "https://www.mairie-francheville69.fr/wp-content/uploads/2017/11/image-test-1.jpeg"
-  #   fill_in "legend", with: "Image Legend Test"
-  #   fill_in "rating", with: "5"
-  #   click_on 'Create Image'
-  #   # Should be redirected to Home with new Image
-  #   assert_equal root_path, page.current_path
-  #   assert_selector "Image Title Test"
-  # end
+  test "lets a signed in user create a new product with a file" do
+    login_as users(:baptiste)
+    visit "/images/new"
+    fill_in "title", with: "Image Title Test"
+    fill_in "path", with: "https://www.mairie-francheville69.fr/wp-content/uploads/2017/11/image-test-1.jpeg"
+    fill_in "legend", with: "Image Legend Test"
+    fill_in "rating", with: "5"
+    click_on 'Create Image'
+    # Should be redirected to Home with new Image
+    assert_equal root_path, page.current_path
+    assert_selector "Image Title Test"
+  end
 end
 
